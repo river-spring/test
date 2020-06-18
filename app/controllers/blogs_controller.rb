b79c894dd74aabe57697e968148bcb84a858465d
river-spring <rivermspring@gmail.com>
2020-06-18 06:00:04 +0000

    [Start]4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,6 +25,12 @@ def update
   blog.update(blog_params)
   redirect_to blog_path(blog)
 end
+def destroy
+  blog = Blog.find(params[:id])
+  blog.destroy
+  redirect_to blogs_path
+end
+  end
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
